088437e42c85bc1c923b4bf9044e3958d68040d3
Aryan Mali <aryan.mali951@deccansociety.org>
2024-08-28 01:42:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 import About from "./components/About";
 import Potfolio from "./components/Potfolio";
 import Experience from "./components/Experience";
 import Footer from "./components/footer";
 import Contact from "./components/Contact";
 import { Toaster } from "react-hot-toast";
+import Home from "./components/home";
 
 const App = () => {
 	return (
