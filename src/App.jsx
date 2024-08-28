60527e81668206d093cf4c7a276ef4b9def81da1
Aryan Mali <aryan.mali951@deccansociety.org>
2024-08-28 15:16:19 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Potfolio from "./components/Potfolio";
 import Experience from "./components/Experience";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import { Toaster } from "react-hot-toast";
 import Home from "./components/home";
